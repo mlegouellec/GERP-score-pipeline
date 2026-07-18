d50df49a8b93fc05acf497b18a57bbc69af2e2b3
mlegouellec <mael.legouellec2@gmail.com>
2026-07-18 16:36:26 +0200

    Update 01_GenomesAlignment.sh

--- 01_GenomesAlignment.sh
+++ 01_GenomesAlignment.sh
@@ -17,12 +17,20 @@ RefGenome=<PATH_REFERENCE_GENOME>  #.fa, .fna, .fa.gz or .fna.gz
 GerpGenome=<PATH_GERP_GENOME> #.fa, .fna, .fa.gz or .fna.gz
 outdir=<PATH_OUTPUT> # .paf format
 
+
 # Load packages
 
 ## For instruction on how to use minimap2, refer to 'https://github.com/lh3/minimap2'
 MINIMAP=<PATH_MINIMAP2_FUNCTION> #Or just load the module, depending on how you installed minimap2
+TRANSANNO=<PATH_TRANSANNO_FUNCTION> #Get it at 'https://github.com/informationsea/transanno'
 
 # Run minimap
 
 $MINIMAP -cx asm5 $GerpGenome $RefGenome > ${outdir}
-#-cx asm5 allows to align sequences with ~0.1% divergence or less (closely related species). Adapt this argument to the divergence time between your genomes 
+#-cx asm5 allows to align sequences with ~0.1% divergence or less (closely related species). Adapt this argument to the divergence time between your genomes
+
+# Convert .paf into .chain
+
+$TRANSANNO minimap2chain $outdir --output ${outdir}.chain
+
+
