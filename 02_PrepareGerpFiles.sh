ca76a4343cc269c2fbdb25daca160c667a0c39dc
mlegouellec <mael.legouellec2@gmail.com>
2026-07-19 19:30:44 +0200

    Refactor script with improved comments and new variables
    Updated comments and variable descriptions for clarity. Added new variables for polarized alleles and lifted coordinates.

--- 02_PrepareGerpFiles.sh
+++ 02_PrepareGerpFiles.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 #SBATCH --partition=def
-#SBATCH --qos=mesopsl1_def_long
 #SBATCH --nodes=1
 #SBATCH -V
 #SBATCH -o Chain2bed.o
@@ -18,19 +17,23 @@ LiftOver=<PATH_TRANSANNO_FUNCTION> # Dowload it here : ['https://hgdownload.soe.
 
 # Variables
 
-BIGWIGFILE=<PATH_BIGWIG> # The BigWigFile contains the GERP score of the closest relative of our target species for which such information is available. Download : ['https://ftp.ensembl.org/pub/current/compara/conservation_scores/92_mammals.gerp_conservation_score/']
+BIGWIGFILE=<PATH_BIGWIG> # The BigWigFile contains the GERP score of the closest relative of our target species for which such information is available. Download it here : ['https://ftp.ensembl.org/pub/current/compara/conservation_scores/92_mammals.gerp_conservation_score/']
 BEDGRAPH=<PATH_BIGWIG_BED> # Turn the BigWigFile into a .bed file
-BED_DATA=<PATH_DATASET_BED> # polarized vcf turned into a bed, only with the positions. The total VCF is too heavy, we only use the coordinates from the target genome to make the link with GERP values from the closely related genome
-ChainFile=
+BED_DATA=<PATH_DATASET_BED> # vcf turned into a bed, only with the positions. The total VCF is too heavy, we only use the coordinates from the target genome to make the link with GERP values from the closely related genome. Restrict the VCF to the positions that can be accurately polarized
+BED_POL=<PATH_POLARIZATION_DATFRAME> # Bedfile with polarized alleles : Chrom, Pos-1, Pos, Ancestral Allele
+ChainFile=<PATH_OUTPUT>.paf
+Lifted_Coor=<PATH_LIFTED_COORDINATES_BED> # This is a .bed format
+UnmappedCoor=<PATH_UNLIFTED_COORDINATES_BED> # Unmapped regions from the original coordinates, .bed format
+
 # Command lines
 
 $bigWigToBedGraph $BIGWIGFILE $BEDGRAPH ## Change the BIGWIG file into a .bed
 
-$LiftOver $BED_DATA $ChainFile ${IDQUERY}coor_polar_LiftedOver${IDREF}.bed ${IDQUERY}coor_polar_LiftedOver${IDREF}_UNMAPPED.bed
-#Get Both coordinates in the bed
-#awk '{print $1 "\t" $2 "\t" $3 "\t" $4 "\t" $5 "\t" $5+1}' ${IDQUERY}coor_polar_LiftedOver${IDREF}.bed > ${IDQUERY}coor_polar_LiftedOver${IDREF}_format.bed
-#Get Gerp Score
-bedtools intersect -a ${IDQUERY}coor_polar_LiftedOver${IDREF}_format.bed -b $BEDGRAPH -wa -wb | cut -f4-10 > ${IDQUERY}coor_polar_LiftedOver${IDREF}_GerpScor.bed
-#Get Polar Values
-bedtools intersect -a ${IDREF}on${IDQUERY}coor_polar.bed -b  ${IDQUERY}coor_polar_LiftedOver${IDREF}_GerpScor.bed -wa -wb  > ${IDQUERY}coor_polar_LiftedOver${IDREF}_GerpScor_final.bed
+$LiftOver $BED_DATA $ChainFile $Lifted_Coor $UnmappedCoor ## Lifts over the coordinates of the target genome on the GERP score genome. 
+
+awk '{print $1 "\t" $2 "\t" $3 "\t" $4 "\t" $5 "\t" $5+1}' $Lifted_Coor > $Lifted_Coor.format.bed ## Correct format for the .bed
+
+bedtools intersect -a $Lifted_Coor.format.bed -b $BEDGRAPH -wa -wb | cut -f4-10 > $Lifted_Coor.format.gerp.bed ## Get the GERP scores for the lifted over positions
+
+bedtools intersect -a $BED_POL -b  $Lifted_Coor.format.gerp.bed -wa -wb  > $Lifted_Coor.format.gerp.final.bed ## Associate GERP scores and Ancestral allele in the target genome
 
