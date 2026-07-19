b2fbc8b0381607021aa531fa82a1609a60fde484
mlegouellec <mael.legouellec2@gmail.com>
2026-07-19 18:11:33 +0200

    Update paths and commands in PrepareGerpFiles script

--- 02_PrepareGerpFiles.sh
+++ 02_PrepareGerpFiles.sh
@@ -13,25 +13,24 @@
 # Modules and functions
 module load gcc/9.2.0
 module load samtools/1.10
-bigWigToBedGraph=<PATH_bigWigToBedGraph_FUNCTION> # Dowload it here : ['https://ftp.ensembl.org/pub/current/compara/conservation_scores/92_mammals.gerp_conservation_score/']
-TRANSANNO=<PATH_TRANSANNO_FUNCTION> # Dowload it here : ['https://hgdownload.soe.ucsc.edu/admin/exe/linux.x86_64/']
+bigWigToBedGraph=<PATH_bigWigToBedGraph_FUNCTION> # Dowload it here : ['https://hgdownload.soe.ucsc.edu/admin/exe/linux.x86_64/']
+LiftOver=<PATH_TRANSANNO_FUNCTION> # Dowload it here : ['https://hgdownload.soe.ucsc.edu/admin/exe/linux.x86_64/']
 
 # Variables
 
-BIGWIGFILE=<PATH_BIGWIG> # The BigWigFile contains the GERP score of the closest relative of our target species for which such information is available. Download : 
-BEDGRAPH=<'PATH_BIGWIG_BED'> # Turn the BigWigFile into a .bed file
-cd $WD
+BIGWIGFILE=<PATH_BIGWIG> # The BigWigFile contains the GERP score of the closest relative of our target species for which such information is available. Download : ['https://ftp.ensembl.org/pub/current/compara/conservation_scores/92_mammals.gerp_conservation_score/']
+BEDGRAPH=<PATH_BIGWIG_BED> # Turn the BigWigFile into a .bed file
+BED_DATA=<PATH_DATASET_BED> # polarized vcf turned into a bed, only with the positions. The total VCF is too heavy, we only use the coordinates from the target genome to make the link with GERP values from the closely related genome
+ChainFile=
+# Command lines
 
-bigWigToBedGraph $BIGWIGFILE $BEDGRAPH
+$bigWigToBedGraph $BIGWIGFILE $BEDGRAPH ## Change the BIGWIG file into a .bed
 
-#$TRANSANNO chain-to-bed-vcf $CHAIN --output-query-bed ${IDQUERY}_coor.bed --query $REF --output-query-vcf REF_DIFF_RvQ.vcf.gz --output-reference-bed ${IDREF}_coor.bed --reference $QUERY --output-reference-vcf QUERY_DIFF_RvQ.vcf.gz
+$LiftOver $BED_DATA $ChainFile ${IDQUERY}coor_polar_LiftedOver${IDREF}.bed ${IDQUERY}coor_polar_LiftedOver${IDREF}_UNMAPPED.bed
+#Get Both coordinates in the bed
+#awk '{print $1 "\t" $2 "\t" $3 "\t" $4 "\t" $5 "\t" $5+1}' ${IDQUERY}coor_polar_LiftedOver${IDREF}.bed > ${IDQUERY}coor_polar_LiftedOver${IDREF}_format.bed
+#Get Gerp Score
+bedtools intersect -a ${IDQUERY}coor_polar_LiftedOver${IDREF}_format.bed -b $BEDGRAPH -wa -wb | cut -f4-10 > ${IDQUERY}coor_polar_LiftedOver${IDREF}_GerpScor.bed
+#Get Polar Values
+bedtools intersect -a ${IDREF}on${IDQUERY}coor_polar.bed -b  ${IDQUERY}coor_polar_LiftedOver${IDREF}_GerpScor.bed -wa -wb  > ${IDQUERY}coor_polar_LiftedOver${IDREF}_GerpScor_final.bed
 
-#cat ${IDREF}_coor.bed | awk '{print $1 "\t" $2 "\t" $3 "\t" $4}' | awk -F "\tchain_id:" '{print $1 "\t" $2}' | sed 's/;/\t/g' | sed 's/:/\t/g' | sed 's/-/\t/g' | awk '{print $5 "\t" $6 "\t" $7 "\t" $1 "\t" $2 "\t" $3}' > ${IDQUERY}on${IDREF}coor.bed
-
-#cat ${IDQUERY}on${IDREF}coor.bed |cut -f4,5,6 > ${IDQUERY}on${IDREF}Sub.bed
-
-#bedtools makewindows -b ${IDQUERY}on${IDREF}Sub.bed -w 1 > ${IDQUERY}on${IDREF}OnlyPerPosition.bed
-
-
-
-#bedtools intersect -a $BEDGRAPH -b ${IDQUERY}on${IDREF}coor.bed -wa  > IntersectBigWig${IDQUERY}on${IDREF}.bed
