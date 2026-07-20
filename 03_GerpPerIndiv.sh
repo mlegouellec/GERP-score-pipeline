fceb394522f482a3d0e502ee6b1a2d006e10d31d
mlegouellec <mael.legouellec2@gmail.com>
2026-07-20 12:48:26 +0200

    Enhance script with comments and data processing
    Added comments for clarity and improved data filtering steps.

--- 03_GerpPerIndiv.sh
+++ 03_GerpPerIndiv.sh
@@ -38,13 +38,23 @@ bcftools view -R   ${FinalBed}_${Scaff}.bed $VCF_INPUT -Oz -o $VCF_OUTPUT
 tabix  $VCF_OUTPUT
 
 for Indiv in $(cat $ListeIndivs); do
+        
         mkdir ${Indiv_dir}${Indiv}
         echo ${Indiv}
-        i=$(cat -n $ListeIndivs | grep $Indiv | cut -f1)
+        i=$(cat -n $ListeIndivs | grep $Indiv | cut -f1) # Get the index of the individual in the VCF. It's why it's important that the list follows the same organization as the VCF.
         echo $i # CAUTION : Check if the the indiv and its index are matching
+        
         zgrep -v '#'  $VCF_OUTPUT | \
                 awk -v colIndiv="$i" 'BEGIN {OFS="\t"}{print $1,$2-1,$2,$3,$4,$5,substr($(colIndiv+9),1,1),substr($(colIndiv+9),3,1)}' | \ # Extract Chrom, Pos-1, Pos, ID, Ref, Alt, Genotype1~Indiv, Genotype2~Indiv 
                 awk '$7 != "." && $8 != "."' | awk 'length($5)+length($6) == 2' > \  # Filter sites for which we have no missing data, INDELS and more than 2 alt (This should have been filtered already ! it's just an extra security)
                 ${Indiv_dir}${Indiv}/Scaffold_${Scaff}_${Indiv}_${IDpop}.bed
-
+                
+        bedtools intersect -a ${FinalBed}_${Scaff}.bed  -b  ${Indiv_dir}${Indiv}/Scaffold_${Scaff}_${Indiv}_${IDpop}.bed -wa -wb | \ # Get the GERP scores for the .bed we obtained
+        awk 'BEGIN {OFS="\t"}{print $13,$14,$15,$17,$18,$19,$20,$5,$12}' | \ # Selected columns : 'Scaff, Pos-1, Pos, Ref, Alt, Genotype1, Genotype2, Ancestral, Gerp score'
+        awk 'BEGIN {OFS="\t"}{if ($4 != $8) {
+                $6 = ($6 - 1)^2
+                $7 = ($7 - 1)^2
+                }print}' > \ # If the Ref != Ancestral, then in the genotype section 0 becomes 1 and 1 becomes 0. CAUTION : It's only true if we don't have more than 2 alleles at this position. Make sure your polarization framework excludes positions with more than 2 alleles for REF+Outgroups, or add a condition here '($5 == $8)'
+        ${Indiv_dir}${Indiv}/Scaffold_${Scaff}_${Indiv}_${IDpop}_GERP.bed
+        
 done
